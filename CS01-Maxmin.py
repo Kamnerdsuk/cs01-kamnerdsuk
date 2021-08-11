02ca186f0f4d8bf77c4fa9284b0329730af93de3
Kamnerdsuk <87053922+Kamnerdsuk@users.noreply.github.com>
2021-08-11 12:05:54 +0700

    Create CS01-Maxmin.py

---
+++ CS01-Maxmin.py
@@ -0,0 +1,14 @@
+nub = int(input())
+list = []
+for i in range(nub):
+    num = input()
+    if num == "":break
+    list.append(int(num))
+print(list)
+print(min(list))
+print(max(list))
+
+# เป็นโสดทำไม   อยู่ไปให้เศร้าเหงาทรวง
+# ไม่คิดจะหาคู่ควง   เดี๋ยวจะล่วงพ้นวัยไปเปล่า
+# เกิดมาเดียวดาย   จะตายเพราะความเหงาเศร้า
+# แต่งงานกันเสียเถิดเรา   อยู่ว่างเปล่าไม่มีอะไร
